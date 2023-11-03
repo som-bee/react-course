d200db08a95bb583e5f9a6e8b7e1ccc8a9ebb73b
Somnath Bera <somnathbera1919@gmail.com>
2023-11-03 16:02:06 +0530

    light and dark theme

--- 03reacttailwind/src/App.jsx
+++ 03reacttailwind/src/App.jsx
@@ -1,18 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bgColor, setbgColor] = useState("white");
+  const [textColor, settextColor] = useState("black");
 
   return (
     <>
-     <h1 className="text-3xl rounded bg-slate-50 text-black p-4">
-      Hello world!
-    </h1>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: bgColor, color: textColor }}
+      >
+        <div className="p-6">
+          <h1 className="text-3xl">Content</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            nostrum nulla est veniam debitis ratione dolore facere vel quibusdam
+            eius nemo, enim voluptate rerum minima autem perferendis nesciunt.
+            Molestiae, quia!
+          </p>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            nostrum nulla est veniam debitis ratione dolore facere vel quibusdam
+            eius nemo, enim voluptate rerum minima autem perferendis nesciunt.
+            Molestiae, quia!
+          </p>
+        </div>
+
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div
+            className="flex flex-wrap justify-center gap-3 shadow-lg  px-2 py-2 rounded-3xl"
+            style={{ backgroundColor: "white" }}
+          >
+            <button
+              onClick={() => {
+                setbgColor("black");
+                settextColor("white");
+              }}
+              className="outline-none px-4 py-1 rounded-full  shadow-lg duration-200"
+              style={{ backgroundColor: bgColor, color: textColor }}
+            >
+              Dark
+            </button>
+            <button
+              onClick={() => {
+                setbgColor("white");
+                settextColor("black");
+              }}
+              className="outline-none px-4 py-1 rounded-full  shadow-lg duration-200"
+              style={{ backgroundColor: textColor, color: bgColor }}
+            >
+              Light
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
